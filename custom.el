95354d741d952d0c96ffb4b92a1395aff6104a87
Vighnesh <vvijay@roblox.com>
2023-06-27 10:53:59 -0700

    fix up init files

---
+++ custom.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(display-time-24hr-format t)
+ '(display-time-default-load-average nil)
+ '(display-time-mode t)
+ '(mode-line-position-column-line-format '(" %l:%c"))
+ '(package-selected-packages
+   '(avy orderless change-inner rg consult-eglot projectile evil rust-mode diff-hl magit corfu eglot consult vertico)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mode-line ((t (:background "gold1" :foreground "gray0"))))
+ '(mode-line-buffer-id ((t (:weight normal))))
+ '(mode-line-emphasis ((t (:weight bold))))
+ '(mode-line-highlight ((t nil)))
+ '(mode-line-inactive ((t nil))))
